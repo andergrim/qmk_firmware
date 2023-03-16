86d72c284f3ba34ceb19d7043da165a45b64285e
Kristoffer Andergrim <kristoffer.andergrim@wirelesscar.com>
2023-03-16 11:00:28 +0100

    numpad layer

--- keyboards/beekeeb/piantor/keymaps/andergrim/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/andergrim/keymap.c
@@ -13,12 +13,18 @@
 #define GUI__O RGUI_T(KC_O)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    //        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    // LCTL(KC_ESC), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL_T(KC_SCLN),
+    //      KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
+    //                        KC_LGUI, KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
+
     // Colemak DH
     [0] = LAYOUT_split_3x6_3(
                 KC_TAB,   KC_Q,   KC_W,   KC_F,   KC_P, KC_B,     KC_J, KC_L,   KC_U,    KC_Y,   KC_MINS, KC_BSPC,
         LCTL_T(KC_ESC), GUI__A, ALT__R, SFT__S, CTL__T, KC_G,     KC_M, CTL__N, SFT__E,  ALT__I, GUI__O,  LCTL_T(KC_SCLN),
                KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_D, KC_V,     KC_K, KC_H,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                                      KC_LGUI, MO(1), KC_SPC,     KC_ENT, MO(2), KC_LALT
+                                        MO(4), MO(1), KC_SPC,     KC_ENT, MO(2), KC_LALT
     ),
 
     // Numerics and navigation
@@ -26,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,  KC_5,     KC_6,    KC_7,    KC_8,  KC_9,    KC_0,    KC_DEL,
         LCTL(KC_ESC), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_PGUP,
              KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,     KC_NO,   KC_RBRC, KC_NO, KC_NO,   KC_END,  KC_PGDN,
-                                       KC_LGUI, KC_TRNS, KC_SPC,     KC_ENT, MO(3), KC_LGUI
+                                         MO(4), KC_TRNS, KC_SPC,     KC_ENT, MO(3), KC_LGUI
     ),
 
     // Shifted chars
@@ -34,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_TAB,    KC_EXLM, KC_AT, KC_HASH, RALT(KC_4), KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,       KC_RPRN,       KC_BSPC,
         LCTL(KC_ESC), RALT(KC_2), KC_NO,   KC_NO,      KC_NO,   KC_NO,     KC_GRV,  KC_EQL,  KC_NO,   KC_RBRC,       KC_BSLS,       KC_MINS,
              KC_LSFT,    KC_NUBS, KC_NO,   KC_NO,      KC_NO,   KC_NO,     KC_UNDS, KC_PLUS, KC_NO,   RALT(KC_NUBS), RALT(KC_MINS), KC_RSFT,
-                                               KC_LALT, MO(3), KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
+                                                 MO(4), MO(3), KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
     ),
 
     // F-keys
@@ -42,7 +48,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5, KC_F6,     KC_F7, KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
         LCTL(KC_ESC), GUI__A, ALT__R, SFT__S, CTL__T, KC_NO,     KC_NO, CTL__N, SFT__E, ALT__I, GUI__O, LCTL_T(KC_SCLN),
              KC_LSFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_RSFT,
-                                   KC_LGUI, KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
+                                     MO(4), KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
+    )
+
+    // Numpad
+	[4] = LAYOUT_split_3x6_3(
+               KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_BSPC,
+        LCTL(KC_ESC), GUI__A, ALT__R, SFT__S, CTL__T, KC_NO,     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, LCTL_T(KC_SCLN),
+             KC_LSFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_P0,   KC_P1, KC_P2, KC_P3, KC_PDOT, KC_RSFT,
+                                   KC_TRNS, KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
     )
 };
 
