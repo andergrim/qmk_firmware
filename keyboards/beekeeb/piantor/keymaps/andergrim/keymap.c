551efdf78f431f9178f07575e4c3d33fdaafd53d
Kristoffer Andergrim <kristoffer.andergrim@wirelesscar.com>
2023-03-16 16:23:58 +0100

    added numlock

--- keyboards/beekeeb/piantor/keymaps/andergrim/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/andergrim/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Numpad
 	[4] = LAYOUT_split_3x6_3(
                KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_BSPC,
-        LCTL(KC_ESC), GUI__A, ALT__R, SFT__S, CTL__T, KC_NO,     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, LCTL_T(KC_SCLN),
+        LCTL(KC_ESC), GUI__A, ALT__R, SFT__S, CTL__T, KC_NO,     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, LCTL_T(KC_NUM),
              KC_LSFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_P0,   KC_P1, KC_P2, KC_P3, KC_PDOT, KC_RSFT,
                                    KC_TRNS, KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
     )
