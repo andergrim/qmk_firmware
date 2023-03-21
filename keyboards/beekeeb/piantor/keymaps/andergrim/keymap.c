976dc1559b8e6686abaaa3852bc1974098746db5
Kristoffer Andergrim <kristoffer.andergrim@wirelesscar.com>
2023-03-21 21:32:45 +0100

    Fix TT for layer itself

--- keyboards/beekeeb/piantor/keymaps/andergrim/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/andergrim/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_BSPC,
         LCTL(KC_ESC), GUI__A, ALT__R, SFT__S, CTL__T, KC_NO,     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, LCTL_T(KC_NUM),
              KC_LSFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_P0,   KC_P1, KC_P2, KC_P3, KC_PDOT, KC_RSFT,
-                                     KC_NO, KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
+                                     TT(4), KC_TRNS, KC_SPC,     KC_ENT, KC_TRNS, KC_LALT
     )
 };
 
