9ebe0cc27e91f1b7b6dc06cfa5cf3aeefc4023d3
kristoffer <kristoffer.andergrim@logikfabriken.se>
2024-03-14 15:59:04 +0100

    Add TAB to numpad layer

--- keyboards/crkbd/keymaps/andergrim/keymap.c
+++ keyboards/crkbd/keymaps/andergrim/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Numpad
 	[4] = LAYOUT_split_3x6_3(
-               KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_BSPC,
+              KC_TAB,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_BSPC,
             LCTL_ESC, GUI__A, ALT__R, SFT__S, CTL__T, KC_NO,     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, LCTL_T(KC_NUM),
              KC_LSFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,     KC_P0,   KC_P1, KC_P2, KC_P3, KC_PDOT, KC_RSFT,
                                        TT(4), MO(1), KC_SPC,     MO(2), KC_TRNS, KC_LALT
