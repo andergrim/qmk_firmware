4d15305796254f525dba26cc7235a35968666bc9
kristoffer <kristoffer.andergrim@logikfabriken.se>
2024-03-19 00:36:37 +0100

    Remove IGNORE_MOD_TAP_INTERRUPT since it is now default behaviour from upstream.

--- keyboards/crkbd/keymaps/andergrim/config.h
+++ keyboards/crkbd/keymaps/andergrim/config.h
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MASTER_LEFT
 
-#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD // https://precondition.github.io/home-row-mods#tapping-force-hold
 #define TAPPING_TERM 150
 #define TAPPING_TOGGLE 3
